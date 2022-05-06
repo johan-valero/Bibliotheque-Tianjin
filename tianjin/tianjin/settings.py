4bbe7f2425907076c781039d035747890f64b575
Johan Valero <valero_johan@hotmail.fr>
2022-05-06 16:35:49 +0200

    add files

--- tianjin/tianjin/settings.py
+++ tianjin/tianjin/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/library/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/library/')
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
