1758e46baa8f560a5c21d3c5a55ad1e75ad5924e
johan-valero <valero_johan@hotmail.fr>
2022-05-06 20:12:44 +0200

    add

--- tianjin/tianjin/urls.py
+++ tianjin/tianjin/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('library/', include('library.urls')),
     path('admin/', admin.site.urls),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+#+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
