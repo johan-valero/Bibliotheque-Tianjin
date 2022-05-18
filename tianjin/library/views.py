82376653f44a48ced49ad679aa9c3e0a9da4637f
Johan Valero <valero_johan@hotmail.fr>
2022-05-18 16:26:06 +0200

    update files

--- tianjin/library/views.py
+++ tianjin/library/views.py
@@ -4,6 +4,7 @@ from . models import Author, Category, Langage, Book
 from django import template
 from django.template.loader import get_template 
 from django.template import loader
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -83,23 +84,64 @@ def list_author(request, author_id):
 
 def resultat(request):
     context = {}
+    books_name = []
+    books_firstname = []
+    books_date = []
+    books_author = []
+    books_language = []
+    books_category= []
+
     if request.method == "POST":
         search_input = request.POST['search_input'].lower()
-        books = Book.objects.filter(name_book__contains=search_input)
+        words = search_input.split(" ")
+        
+        # word_list = search_input.split(" ")
+        # for word in word_list:
+        #     a = Book.objects.filter(Q(name_book__contains=word) | Q(date_book__contains=word) | Q(author__name_author__contains=word) | Q(author__firstname_author__contains=word) | Q(language__name_langage__contains=word) | Q(category__name_category__contains=word) )
+        #     for i in a : resultats.append(a)
+    
 
-        books_date = Book.objects.filter(date_book__contains=search_input)
+        for word in words:
+            books = Book.objects.filter(name_book__contains=word)
+            for i in books :
+                books_name.append(i)
 
-        books_author = Book.objects.filter(author__name_author__contains=search_input)
+        for word in words:
+            books = Book.objects.filter(date_book__contains=word)
+            for i in books :
+                books_date.append(i)
 
-        books_firstname = Book.objects.filter(author__firstname_author__contains=search_input)
+        for word in words:
+            books = Book.objects.filter(author__name_author__contains=word)
+            for i in books :
+                books_author.append(i)
 
-        books_language = Book.objects.filter(language__name_langage__contains=search_input)
+        for word in words:  
+            books = Book.objects.filter(author__firstname_author__contains=word)
+            for i in books :
+                books_firstname.append(i)
 
-        books_category = Book.objects.filter(category__name_category__contains=search_input)
+        for word in words:  
+            books = Book.objects.filter(language__name_langage__contains=word)
+            for i in books :
+                books_language.append(i)
+
+        for word in words:  
+            books = Book.objects.filter(category__name_category__contains=word)
+            for i in books :
+                books_category.append(i)
+
+        # #filtre les resultats en resultat unique 
+        books_name = list(set(books_name))
+        books_date = list(set(books_date))
+        books_author = list(set(books_author))
+        books_firstname = list(set(books_firstname))
+        books_language = list(set(books_language))
+        books_category = list(set(books_category))
 
         context = {
             'search_input': search_input,
-            'books' :books,
+            'books' : books_name,
             'books_firstname': books_firstname,
             'books_date': books_date,
             'books_author': books_author,
